43a46c35615292345916165a205a98eedfdb82eb
Anik0047 <clananik@gmail.com>
2024-07-01 22:53:22 +0600

    working footer page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './Footer.css';
+import youTube from '../../assets/youtube_icon.png'
+import twitter from '../../assets/twitter_icon.png'
+import instagram from '../../assets/instagram_icon.png'
+import facebook from '../../assets/facebook_icon.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className="footer_icons">
-        <img src="" alt="" />
+        <img src={facebook} alt="" />
+        <img src={instagram} alt="" />
+        <img src={twitter} alt="" />
+        <img src={youTube} alt="" />
       </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Preference</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright_text'>&#169; 1997-2024 Netflix, Inc.</p>
     </div>
   )
 }
