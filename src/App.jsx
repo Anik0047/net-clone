e042152db28231ac2b866451f38dbe9e1d3e905f
Anik0047 <clananik@gmail.com>
2024-07-01 23:07:07 +0600

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login'
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>} />
+        <Route path='/login' element={<Login></Login>} />
+      </Routes>
+      
     </div>
   )
 }
