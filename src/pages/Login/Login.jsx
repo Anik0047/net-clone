7100acb6af8baace52912ba1134736a656854517
Anik0047 <clananik@gmail.com>
2024-07-02 22:12:51 +0600

    login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Login.css';
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
     <div className='login'>
-      
+      <img src={logo} alt="" />
+      <div className="login_form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder='Name' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="form_help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
