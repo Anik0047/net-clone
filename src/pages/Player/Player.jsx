e1476970ccca30f7815e385541ebcc2edb403596
Anik0047 <clananik@gmail.com>
2024-07-05 20:16:05 +0600

    media playing page

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Player.css';
+import back_arrow from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
     <div className='player'>
-      
+      <img src={back_arrow} alt="" />
+      <iframe src='https://www.youtube.com/embed/hkHHwA-vEyQ' width='90%' height='90%' title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player_info">
+        <p>Publish date</p>
+        <p></p>
+        <p></p>
+      </div>
     </div>
   )
 }
