1a9dc51d429a52e838ccba6e5349f05d5f263b11
Anik0047 <clananik@gmail.com>
2024-07-06 17:49:37 +0600

    firbase checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 const App = () => {
-  console.log('Firebase API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
+  
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, async(user)=>{
+      if(user){
+        console.log("Logged In");
+        navigate('/');
+      }else{
+        console.log("Logged Out");
+        navigate('/login');
+      }
+    })
+  },[])
+
+
   return (
     <div>
       <Routes>
